7438c100202dead9e09163713bafcb6f853ec1d2
wurststulle <wurststulle@gmail.com>
2011-06-08 22:54:49 +0200

    add content type to agent download

--- build/admin/releases.php
+++ build/admin/releases.php
@@ -10,6 +10,7 @@ if(isset($_REQUEST['getFile'])){
 	$agent = AgentReleaseQuery::create()->findOneById($_REQUEST['getFile']);
 	/* @var $agent AgentRelease */
 	if($agent != null){
+		header("Content-type: application/force-download");
 		header('Content-Disposition: attachment; filename='.'yaffmap_'.$agent->getRelease().'-'.$agent->getSubRelease().'_'.$agent->getVersion().'_'.$agent->getUpgradeTree().'.tar.gz');
 		echo stream_get_contents($agent->getAgent());
 	}else{
