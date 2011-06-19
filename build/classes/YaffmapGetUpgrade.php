1504f288dea847824cacd8e3b793c6b7a7b6b1b7
wurststulle <wurststulle@gmail.com>
2011-06-19 21:30:12 +0200

    rewrite getUpgrade

--- build/classes/YaffmapGetUpgrade.php
+++ build/classes/YaffmapGetUpgrade.php
@@ -4,8 +4,7 @@ class YaffmapGetUpgrade extends Yaffmap{
 	
 	public function __construct($request = null, $response = null){
 		call_user_func_array('parent::__construct', array($request, $response));
-		$allowed = array('version', 'tree', 'rel', 'updateToRel');
-		$this->checkInput($allowed);
+		$this->checkInput();
 	}
 	
 	/**
@@ -14,89 +13,31 @@ class YaffmapGetUpgrade extends Yaffmap{
 	 * @throws YaffmapException
 	 */
 	public function getUpgrade(){
-		if(isset($this->request['version']) && isset($this->request['tree'])){
-			// version and tree given
-			if(isset($this->request['rel']) && !isset($this->request['updateToRel'])){
-				$tmp = explode("-", $this->request['rel']);
-				$relName = $tmp[0];
-				$relSubName = $tmp[1];
-				// installed release given, do not force update to specified release
-				$installedVersion = AgentReleaseQuery::create()
-					->filterByVersion($this->request['version'])
-					->filterByUpgradeTree($this->request['tree'])
-					->filterByRelease($relName)
-					->filterBySubRelease($relSubName)
-					->findOneOrCreate();
-				if(!$installedVersion->getIsHead()){
-					// installed release is not up to date, install head of given tree
-					$updateTo = AgentReleaseQuery::create()
-						->filterByUpgradeTree($this->request['tree'])
-						->filterByIsHead(true)
-						->filterByVersion($this->request['version'])
-						->findOne();
-					if($relName >= $updateTo->getRelease() && $relSubName > $updateTo->getSubRelease()){
-						// installed release is newer then release to be installed
-//						throw new YaffmapLoggedException('i\'ll not downgrade! i\'ll not downgrade!');
-					}else{
-						if($updateTo != null){
-							$this->response->setErrorCode(ResponseCodeNode::OPERATION_SUCCEDED);
-							$this->response->setErrorMsg('Update to release '.$updateTo->getRelease().'-'.$updateTo->getSubRelease().' found.');
-							$this->response->addData('url="'.$updateTo->getUrl().'"');
-						}else{
-							// version or tree not found
-							$this->response->setErrorCode(ResponseCodeNode::OPERATION_FAILED);
-							$this->response->setErrorMsg('No update found.');
-						}
-					}
-				}else{
-					// no update found
-					$this->response->setErrorCode(ResponseCodeNode::OPERATION_SUCCEDED);
-					$this->response->setErrorMsg('Latest release('.$installedVersion->getRelease().'-'.$installedVersion->getSubRelease().') already installed.');
-				}
-			}elseif(!isset($this->request['rel']) && !isset($this->request['updateToRel'])){
-				// installed release not given, install head of given tree
-				$updateTo = AgentReleaseQuery::create()
-					->filterByUpgradeTree($this->request['tree'])
-					->filterByIsHead(true)
-					->filterByVersion($this->request['version'])
-					->findOne();
-				if($updateTo != null){
-					$this->response->setErrorCode(ResponseCodeNode::OPERATION_SUCCEDED);
-					$this->response->setErrorMsg('Update to release '.$updateTo->getRelease().'-'.$updateTo->getSubRelease().' found.');
-					$this->response->addData('url="'.$updateTo->getUrl().'"');
-				}else{
-					// version or tree not found
-					$this->response->setErrorCode(ResponseCodeNode::OPERATION_FAILED);
-					$this->response->setErrorMsg('No update found.');
-				}
-			}elseif(isset($this->request['updateToRel'])){
-				$tmp = explode("-", $this->request['updateToRel']);
-				$relName = $tmp[0];
-				$relSubName = $tmp[1];
-				// force to update to a given release
-				$updateTo = AgentReleaseQuery::create()
-					->filterByVersion($this->request['version'])
-					->filterByRelease($relName)
-					->filterBySubRelease($relSubName)
-					->filterByUpgradeTree($this->request['tree'])
-					->findOneOrCreate();
-				if($updateTo->isNew()){
-					// revision not found, ahhh
-					$this->response->setErrorCode(ResponseCodeNode::OPERATION_FAILED);
-					$this->response->setErrorMsg('Requested release or tree not found. Update failed.');
-				}else{
-					$this->response->setErrorCode(ResponseCodeNode::OPERATION_SUCCEDED);
-					$this->response->setErrorMsg('Update release to '.$updateTo->getRelease().'-'.$updateTo->getSubRelease().' of '.$updateTo->getDevelopmentTree().' tree.');
-					$this->response->addData('url="'.$updateTo->getUrl().'"');	
-				}
+		$tmp = explode("-", $this->request['release']);
+		$relName = $tmp[0];
+		$relSubName = $tmp[1];
+		$latestAgent = null;
+		$latestAgent = AgentRelease::getLatest($this->request['version'], $this->request['tree']);
+		if($latestAgent == null){
+			$latestDefaultAgent = AgentRelease::getLatest($this->request['version'], YaffmapConfig::get('defaultTree'));
+			if($latestDefaultAgent == null){
+				$this->response->setResponseCode(YaffmapResponse::OPERATION_FAILED);
+				$this->response->setResponseMsg('Given version is not known by this backend.');
 			}else{
-				$this->response->setErrorCode(ResponseCodeNode::OPERATION_FAILED);
-				$this->response->setErrorMsg('Update fauled.');
+				$this->response->setResponseCode(YaffmapResponse::NEW_AGENT_RELEASE_FOUND);
+				$this->response->setResponseMsg('Given tree not known by this backend, please install default tree('.$latestDefaultAgent->getUrl().').');
+				$this->response->addResponseData($latestDefaultAgent->getUrl());
 			}
 		}else{
-			// version or tree not given
-			$this->response->setErrorCode(ResponseCodeNode::OPERATION_FAILED);
-			$this->response->setErrorMsg('release or tree not given. Update fauled.');
+			if($latestAgent->getRelease() > $relName || $latestAgent->getSubRelease() > $relSubName){
+				// there is a newer agent, update!
+				$this->response->setResponseCode(YaffmapResponse::NEW_AGENT_RELEASE_FOUND);
+				$this->response->setResponseMsg('New agent release found.');
+				$this->response->addResponseData($latestAgent->getUrl());
+			}else{
+				$this->response->setResponseCode(YaffmapResponse::NEW_AGENT_RELEASE_NOT_FOUND);
+				$this->response->setResponseMsg('No new agent release found.');
+			}
 		}
 		return $this->response;
 	}
--- build/classes/yaffmap/AgentRelease.php
+++ build/classes/yaffmap/AgentRelease.php
@@ -29,4 +29,32 @@ class AgentRelease extends BaseAgentRelease {
 		$ar->agentSize = $this->getAgentSize();
 		return $ar;
 	}
+	
+	/**
+	 * return latest agent release of given tree and version
+	 * 
+	 * @param string $tree
+	 * @param string $version
+	 * @return AgentRelease
+	 */
+	public function getLatest($version, $tree){
+		$con = Propel::getConnection(AgentReleasePeer::DATABASE_NAME);
+		$sql = 'select * from '.AgentReleasePeer::TABLE_NAME.' t1 where t1.version = :version and t1.tree = :tree and t1.release = 
+		(select max(t2.release) from yaffmap_agentRelease t2 where  t2.version = t1.version and t2.tree = t1.tree) 
+		and t1.subrelease = (select max(t3.subRelease) from yaffmap_agentRelease t3 where  t3.version = t1.version and t3.tree = t1.tree)';
+		$stmt = $con->prepare($sql);
+		$stmt->execute(array(':version' => $version, ':tree' => $tree));
+		$formatter = new PropelObjectFormatter();
+		$formatter->setClass('AgentRelease');
+		return $formatter->formatOne($stmt);
+	}
+	
+	/**
+	 * return agent download url
+	 * 
+	 * @return string url
+	 */
+	public function getUrl(){
+		return YaffmapConfig::get('url').'/download/index.php?getFile='.$this->getId();
+	}
 } // AgentRelease
