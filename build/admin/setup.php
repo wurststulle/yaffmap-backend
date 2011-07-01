97ac02d93bbeddc62b30096e6a60223ab87976e5
wurststulle <wurststulle@gmail.com>
2011-07-01 16:35:05 +0200

    setup.php: - remove checking for writeable basedir - add adminUser/Pass input fields

--- build/admin/setup.php
+++ build/admin/setup.php
@@ -25,6 +25,7 @@ function check($var, $out = null){
 
 if(isset($_REQUEST['do']) && $_REQUEST['do'] == 'setConfig'){
 	// TODO save
+	echo 'saving this data is not yet implemented :(<br>';
 }
 echo 'checking for php >= 5.3.2... '.check(strnatcmp(phpversion(),'5.3.2') >= 0);
 echo 'checking for soap... '.check(extension_loaded('soap'));
@@ -32,13 +33,14 @@ echo 'checking for json... '.check(extension_loaded('json'));
 echo 'checking for rrd... '.check(extension_loaded('rrd'));
 echo 'checking for reflection... '.check(extension_loaded('Reflection'));
 echo 'checking for config... '.check($config = ConfigQuery::create()->findOne() != null);
-echo 'base dir writeable... '.check(is_writeable('../../'));
 echo '<br><br>';
 echo 'configuration:'.new KBr();
 $form = new KForm(array(new KAttribute('method', 'post'), new KAttribute('action', 'setup.php?do=setConfig')));
 $table = new KTable(array(new KAttribute('border', '1')));
 $table->addThRow(array('key', 'value'));
 $table->addRow(array('url', new KInputfield(array(new KAttributeName('url'), new KAttributeId('url'), new KAttributeValue(YaffmapConfig::get('url')), new Kattribute('size', '50')))));
+$table->addRow(array('adminUser', new KInputfield(array(new KAttributeName('adminUser'), new KAttributeId('adminUser'), new KAttributeValue(YaffmapConfig::get('adminUser')), new Kattribute('size', '50')))));
+$table->addRow(array('adminPasswd', new KInputfield(array(new KAttributeName('adminPasswd'), new KAttributeId('adminPasswd'), new KAttributeValue(YaffmapConfig::get('adminPasswd')), new Kattribute('size', '50')))));
 $table->addRow(array('defaultTree', new KInputfield(array(new KAttributeName('defaultTree'), new KAttributeId('defaultTree'), new KAttributeValue(YaffmapConfig::get('defaultTree')), new Kattribute('size', '50')))));
 $table->addRow(array('deleteOldNode', new KInputfield(array(new KAttributeName('deleteOldNode'), new KAttributeId('deleteOldNode'), new KAttributeValue(YaffmapConfig::get('deleteOldNode')), new Kattribute('size', '48'))).'h'));
 $table->addRow(array('deleteOldLink', new KInputfield(array(new KAttributeName('deleteOldLink'), new KAttributeId('deleteOldLink'), new KAttributeValue(YaffmapConfig::get('deleteOldLink')), new Kattribute('size', '48'))).'h'));
