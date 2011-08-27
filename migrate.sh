1c66bb91114db936e6802eba7d2d486a5913a769
holger gross <wurststulle@gmail.com>
2011-08-27 15:56:07 +0200

    create symbolic link to propel-gen in migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ ! -f propel-gen ]
+then
+	ln -s build/vendor/Propel/generator/bin/propel-gen propel-gen
+fi
 ln -s runtime-conf.xml buildtime-conf.xml
 ./propel-gen diff
 ./propel-gen migrate
