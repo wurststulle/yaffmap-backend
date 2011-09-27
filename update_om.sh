eb762e2906f27c8af4569b27197005920c8c4126
Holger Gross <wurststulle@gmail.com>
2011-09-27 16:13:18 +0200

    pimp update_om script

--- update_om.sh
+++ update_om.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+INSERT=$1
+
 if [ ! -f propel-gen ] 
 then
   ln -s build/vendor/Propel/generator/bin/propel-gen propel-gen
@@ -7,5 +9,8 @@ fi
 
 ./propel-gen om
 ./propel-gen sql
-./propel-gen insert-sql
+if [ $INSERT = "insert"]
+then
+	./propel-gen insert-sql
+fi
 ./propel-gen convert-conf
